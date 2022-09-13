9c0ba297a28404c17a68f4fe59ea82a5ad63810c
Doniel Acosta <donielacosta1995@gmail.com>
2022-09-13 16:40:45 -0400

    auth con hash

--- server.php
+++ server.php
@@ -1,5 +1,26 @@
 <?php
 
+if(!array_key_exists('HTTP_X_HASH', $_SERVER)||!array_key_exists('HTTP_X_TIMESTAMP', $_SERVER)||!array_key_exists('HTTP_X_UID', $_SERVER)){
+
+  die;
+}
+
+list( $hash, $uid, $timestamp ) = [ $_SERVER['HTTP_X_HASH'], $_SERVER['HTTP_X_UID'], $_SERVER['HTTP_X_TIMESTAMP'] ];
+$secret = 'Sh!! No se lo cuentes a nadie!';
+$newHash = sha1($uid.$timestamp.$secret);
+
+if ( $newHash !== $hash ) {
+	header( 'Status-Code: 403' );
+	
+		echo json_encode(
+			[
+				'error' => "No autorizado. Hash esperado: $newHash, hash recibido: $hash",
+			]
+		);
+		
+		die;
+}
+
 
 /* An array of the resources that are allowed to be used in the API. */
 $allowedResourceTypes = [

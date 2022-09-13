5cbcd842a2c0f071a560de6787ccfd446e904540
Doniel Acosta <donielacosta1995@gmail.com>
2022-09-13 16:50:31 -0400

    creacion de token

--- server.php
+++ server.php
@@ -1,24 +1,28 @@
 <?php
 
-if(!array_key_exists('HTTP_X_HASH', $_SERVER)||!array_key_exists('HTTP_X_TIMESTAMP', $_SERVER)||!array_key_exists('HTTP_X_UID', $_SERVER)){
 
-  die;
+if ( !array_key_exists( 'HTTP_X_TOKEN', $_SERVER ) ) {
+
+	die;
 }
 
-list( $hash, $uid, $timestamp ) = [ $_SERVER['HTTP_X_HASH'], $_SERVER['HTTP_X_UID'], $_SERVER['HTTP_X_TIMESTAMP'] ];
-$secret = 'Sh!! No se lo cuentes a nadie!';
-$newHash = sha1($uid.$timestamp.$secret);
+$url = 'https://'.$_SERVER['HTTP_HOST'].'/auth';
 
-if ( $newHash !== $hash ) {
-	header( 'Status-Code: 403' );
-	
-		echo json_encode(
-			[
-				'error' => "No autorizado. Hash esperado: $newHash, hash recibido: $hash",
-			]
-		);
-		
-		die;
+$ch = curl_init( $url );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, [
+	"X-Token: {$_SERVER['HTTP_X_TOKEN']}",
+]);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+$ret = curl_exec( $ch );
+
+if ( curl_errno($ch) != 0 ) {
+	die ( curl_error($ch) );
+}
+
+if ( $ret !== 'true' ) {
+	http_response_code( 403 );
+
+	die;
 }
 
 
